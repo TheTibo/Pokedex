8703d3b7db09d4fb56255825811acbecdb145ae2
Thibault Zhou <thetibo1@gmail.com>
2023-12-12 20:07:27 +0100

    props

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,35 +1,20 @@
-function PokemonCard() {
-    const pokemon = pokemonList[0]
-    
+function PokemonCard(props) {
+    const {pokemon} = props;
+    console.log(props)
     return(
 <figure>
   {
-    pokemon.imgSrc ? (
-<img src={pokemon.imgSrc} alt={pokemon.name}></img>
-) : ( <p>???</p>
-  )}
+  pokemon.imgSrc ? 
+  (<img src={pokemon.imgSrc} alt={pokemon.name}></img>) : 
+  ( <p>???</p>)
+  
+  }
+
 
 <figcaption>{pokemon.name}</figcaption>
-</figure>)}
+</figure>)
 
-const pokemonList = [
 
-  {
-
-    name: "bulbasaur",
-
-    imgSrc:
-
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-
-  },
-
-  {
-
-    name: "mew",
-
-  },
-
-];
+}
 
 export default PokemonCard;
